a7358863e27b6a35285af5cd0b9ab92e0255b496
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-04-26 00:38:30 +0200

    lifted versions of clojure and active-clojure in project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/active-group/lawrence"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [active-clojure "0.12.0" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.11.10"]
+                 [de.active-group/active-clojure "0.42.2" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.9"]]}})
