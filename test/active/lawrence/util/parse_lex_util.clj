5a36d84b9f79fcca42c28621b27ec0f2865d68d8
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-06-19 15:27:12 +0200

    deleted file accidentally moved to this project

--- test/active/lawrence/util/parse_lex_util.clj
+++ test/active/lawrence/util/parse_lex_util.clj
@@ -1,7 +1,5 @@
 (ns active.lawrence.util.parse-lex-util
-  (:require [active.ephemerol.char-set :refer :all]
-            [active.ephemerol.regexp :refer :all]
-            [active.ephemerol.scanner :refer :all]
+  (:require [active.ephemerol.scanner :refer :all]
             [active.ephemerol.scanner-run :refer :all]
             [active.lawrence.direct-lr :as direct]
             [active.lawrence.grammar :as gr]
@@ -19,28 +17,23 @@
   [scanner sym-name-space]
   (binding [*ns* (find-ns sym-name-space)]
     (eval (scanner->expression scanner))))
+
 (defn parse
   [g m vs]
-  (let [cooked-input (input g vs)]
-    (println cooked-input)
-    (direct/parse g 1 m cooked-input)))
+    (direct/parse g 1 m (input g vs)))
 
 (defn def-complete-scan [sym-name-space this-scanner-spec
                          input]
   (let [scanner (compute-scanner this-scanner-spec)
-        scan-one (make-scan-one (eval-scanner scanner
-                                              sym-name-space))
+        scan-one (make-scan-one (eval-scanner scanner sym-name-space))
         scan-result (scan-to-list
                       scan-one
                       (string->list input)
                       (make-position 1 0))]
     (first scan-result)))
 
-
 (defn execute-direct [sym-name-space this-scanner-spec
                       yacc-result input method]
-  (let [output (apply list (def-complete-scan sym-name-space
-                                              this-scanner-spec
-                                              input))]
-    (parse yacc-result method output                        ;;(first scan-result)
-           )))
+  (let [output (apply list
+                      (def-complete-scan sym-name-space this-scanner-spec input))]
+    (parse yacc-result method output)))
